c33b23c86e33f22112fadd4b088f9e2c64fbe44b
jsquared115 <gryckoj@yahoo.com>
2014-01-29 04:06:18 -0500

    add calendar button to edit date

--- SoftballStatTracker/src/com/example/softballstattracker/AddGameActivity.java
+++ SoftballStatTracker/src/com/example/softballstattracker/AddGameActivity.java
@@ -1,6 +1,7 @@
 package com.example.softballstattracker;
 
 import java.util.List;
+import java.util.Calendar;
 
 import com.example.softballstattracker.DataSources.GameDataSource;
 
@@ -10,8 +11,21 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 
-public class AddGameActivity extends Activity {
+public class AddGameActivity extends Activity implements OnClickListener {
+	
+	 private ImageButton ib;
+	 private Calendar cal;
+	 private int day;
+	 private int month;
+	 private int year;
+	 private EditText et;
 
 	private GameDataSource gameDataSource;
 	
@@ -19,6 +33,13 @@ public class AddGameActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_game);
+		ib = (ImageButton) findViewById(R.id.imageButton1);
+		cal = Calendar.getInstance();
+		day = cal.get(Calendar.DAY_OF_MONTH);
+		month = cal.get(Calendar.MONTH);
+		year = cal.get(Calendar.YEAR);
+		et = (EditText) findViewById(R.id.gameDateInput);
+		ib.setOnClickListener(this);
 		
 		ActionBar actionBar = getActionBar();
 		actionBar.hide();
@@ -26,6 +47,25 @@ public class AddGameActivity extends Activity {
 		gameDataSource = new GameDataSource(this);
 		gameDataSource.open();
 	}
+	
+	@Override
+	 public void onClick(View v) {
+	  showDialog(0);
+	 }
+
+	 @Override
+	 @Deprecated
+	 protected Dialog onCreateDialog(int id) {
+	  return new DatePickerDialog(this, datePickerListener, year, month, day);
+	 }
+
+	 private DatePickerDialog.OnDateSetListener datePickerListener = new DatePickerDialog.OnDateSetListener() {
+		 public void onDateSet(DatePicker view, int selectedYear,
+		 int selectedMonth, int selectedDay) {
+			 et.setText(selectedDay + " / " + (selectedMonth + 1) + " / "
+			 + selectedYear);
+		 }
+	 };
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
