b276687346c90851ca5f4cdc8b15186e24a801a0
jeff <jheffer44@gmail.com>
2014-02-12 15:57:56 -0500

    Forgot to commit generated file

--- SoftballStatTracker/gen/com/example/softballstattracker/R.java
+++ SoftballStatTracker/gen/com/example/softballstattracker/R.java
@@ -8,9 +8,6 @@
 package com.example.softballstattracker;
 
 public final class R {
-    public static final class array {
-        public static final int players_array=0x7f060000;
-    }
     public static final class attr {
     }
     public static final class dimen {
@@ -27,24 +24,49 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090011;
-        public static final int addGame=0x7f09000c;
-        public static final int addPlayer=0x7f09000b;
-        public static final int button1=0x7f09000a;
-        public static final int chooseplayers=0x7f090000;
-        public static final int gameDateInput=0x7f090002;
-        public static final int gameNameInput=0x7f090005;
-        public static final int imageButton1=0x7f090003;
-        public static final int opponent=0x7f090007;
-        public static final int playerNameInput=0x7f090008;
-        public static final int players_spinner=0x7f09000e;
-        public static final int savePlayerButton=0x7f090009;
-        public static final int spinner_fragment=0x7f09000f;
-        public static final int start=0x7f09000d;
-        public static final int stats_fragment=0x7f090010;
-        public static final int textView1=0x7f090001;
-        public static final int textView2=0x7f090004;
-        public static final int textView3=0x7f090006;
+        public static final int action_settings=0x7f08002a;
+        public static final int addGame=0x7f08000c;
+        public static final int addPlayer=0x7f08000b;
+        public static final int atBats=0x7f08000e;
+        public static final int atBatsInput=0x7f080010;
+        public static final int atBatsLabel=0x7f08000f;
+        public static final int beersDrank=0x7f080023;
+        public static final int beersDrankInput=0x7f080025;
+        public static final int beersDrankLabel=0x7f080024;
+        public static final int button1=0x7f08000a;
+        public static final int chooseplayers=0x7f080000;
+        public static final int doubles=0x7f080014;
+        public static final int doublesInput=0x7f080016;
+        public static final int doublesLabel=0x7f080015;
+        public static final int gameDateInput=0x7f080002;
+        public static final int gameNameInput=0x7f080005;
+        public static final int homeRuns=0x7f08001a;
+        public static final int homeRunsInput=0x7f08001c;
+        public static final int homeRunsLabel=0x7f08001b;
+        public static final int imageButton1=0x7f080003;
+        public static final int opponent=0x7f080007;
+        public static final int playerNameInput=0x7f080008;
+        public static final int players_spinner=0x7f080027;
+        public static final int putOuts=0x7f080020;
+        public static final int putOutsInput=0x7f080022;
+        public static final int putOutsLabel=0x7f080021;
+        public static final int rbis=0x7f08001d;
+        public static final int rbisInput=0x7f08001f;
+        public static final int rbisLabel=0x7f08001e;
+        public static final int savePlayerButton=0x7f080009;
+        public static final int saveStats=0x7f080026;
+        public static final int singles=0x7f080011;
+        public static final int singlesInput=0x7f080013;
+        public static final int singlesLabel=0x7f080012;
+        public static final int spinner_fragment=0x7f080028;
+        public static final int start=0x7f08000d;
+        public static final int stats_fragment=0x7f080029;
+        public static final int textView1=0x7f080001;
+        public static final int textView2=0x7f080004;
+        public static final int textView3=0x7f080006;
+        public static final int triples=0x7f080017;
+        public static final int triplesInput=0x7f080019;
+        public static final int triplesLabel=0x7f080018;
     }
     public static final class layout {
         public static final int activity_add_game=0x7f030000;
@@ -57,12 +79,12 @@ public final class R {
         public static final int stats_activity_fragment=0x7f030007;
     }
     public static final class menu {
-        public static final int add_game=0x7f080000;
-        public static final int add_player=0x7f080001;
-        public static final int choose_players=0x7f080002;
-        public static final int main=0x7f080003;
-        public static final int players_selected=0x7f080004;
-        public static final int start=0x7f080005;
+        public static final int add_game=0x7f070000;
+        public static final int add_player=0x7f070001;
+        public static final int choose_players=0x7f070002;
+        public static final int main=0x7f070003;
+        public static final int players_selected=0x7f070004;
+        public static final int start=0x7f070005;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -70,13 +92,20 @@ public final class R {
         public static final int addPlayer=0x7f05000a;
         public static final int addStats=0x7f05000c;
         public static final int app_name=0x7f050000;
+        public static final int at_bats=0x7f050014;
+        public static final int beers_drank=0x7f05001b;
         public static final int choosePlayersGame=0x7f05000d;
         public static final int choose_players=0x7f050007;
         public static final int datePlayed=0x7f05000e;
+        public static final int doubles=0x7f050016;
         public static final int gameName=0x7f050010;
         public static final int hello_world=0x7f050003;
+        public static final int homeruns=0x7f050018;
         public static final int mainMenu=0x7f050009;
+        public static final int put_outs=0x7f05001a;
+        public static final int rbis=0x7f050019;
         public static final int shortDescription=0x7f05000f;
+        public static final int singles=0x7f050015;
         public static final int start=0x7f050008;
         public static final int text_label=0x7f050012;
         public static final int title_activity_add_game=0x7f050005;
@@ -85,6 +114,7 @@ public final class R {
         public static final int title_activity_main_fragment=0x7f050013;
         public static final int title_activity_players_selected=0x7f050011;
         public static final int title_activity_start=0x7f050002;
+        public static final int triples=0x7f050017;
     }
     public static final class style {
         /** 
@@ -108,10 +138,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f060001;
     }
 }
